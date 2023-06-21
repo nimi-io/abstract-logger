d079de29cf5ef40a97fb1354604260fd895859bb
Toluwanimi <85632246+nimi-io@users.noreply.github.com>
2023-06-21 23:49:59 +0100

    dir scraper function

--- Bulder-function/dir-scrapper-function.ts
+++ Bulder-function/dir-scrapper-function.ts
@@ -1,43 +1,38 @@
+import { dir } from "console";
 import fs from "fs";
 
-interface IFileReader {}
-
-abstract class DirectoryScrapper {
-  constructor(public dirPath: string, ) {}
-
-  scanFiles() {
-    return fs
-      .readdirSync(this.dirPath)
-      .reduce<any>((acc: any, file: string) => {
-        if (this.isJSONFile(file)) {
-          acc[file] = this.readJson(`${this.dirPath}/${file}`);
-        } else {
-          acc[file] = this.readText(`${this.dirPath}/${file}`);
-        }
-
-        return acc;
-      }, {});
-  }
-
-  abstract isJSONFile(file: string): boolean;
-  abstract readText(file: string): string;
-  abstract readJson(file: string): unknown;
+interface IFileReader {
+  isJSONFile(file: string): boolean;
+  readText(file: string): string;
+  readJson(file: string): unknown;
 }
 
-class FileReader extends DirectoryScrapper {
+const createDirectoryScraper = (fileReader: IFileReader) => {
+  return (dirPath: string) =>
+    fs.readdirSync(dirPath).reduce<any>((acc: any, file: string) => {
+      if (fileReader.isJSONFile(file)) {
+        acc[file] = fileReader.readJson(`${dirPath}/${file}`);
+      } else {
+        acc[file] = fileReader.readText(`${dirPath}/${file}`);
+      }
+
+      return acc;
+    }, {});
+};
+
+const fileReader: IFileReader = {
   isJSONFile(file: string): boolean {
     return file.endsWith(".json");
-  }
+  },
   readText(file: string): string {
     return fs.readFileSync(file, "utf8").toString();
-  }
+  },
   readJson(file: string): unknown {
     return JSON.parse(fs.readFileSync(file, "utf8").toString());
-  }
-}
-
-const fileReader = new FileReader("./data");
+  },
+};
 
 
+const dirScrapper = createDirectoryScraper(fileReader);
 
-console.log(fileReader.scanFiles());
+console.log(dirScrapper("./data"));
